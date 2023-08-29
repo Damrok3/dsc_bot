ed30b8ac8b76f57ea010ef0e0047047c7bb516cd
Damrok <ckm7667@gmail.com>
2023-08-29 03:58:56 +0200

    on_message event test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from apikeys import *
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 client = commands.Bot(command_prefix='!', intents=intents) # initializing instance of a bot and letting it know that its
                                             # commands start with "!"
 
@@ -11,6 +12,12 @@ client = commands.Bot(command_prefix='!', intents=intents) # initializing instan
 async def on_ready():   # when bot is ready to start receiving commands it executes this function
     print("Initialization complete")
     print("-----------------------")
+
+# @client.event
+# async def on_message(message):
+#     if message.author.id != author id here:
+#         channel = client.get_channel(channel id here)
+#         await channel.send("test")
 
 @client.command()
 async def hello(ctx):
