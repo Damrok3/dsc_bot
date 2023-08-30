20a3efe5a7961d8f589c392a5459b532a753e5da
Damrok <ckm7667@gmail.com>
2023-08-30 07:07:22 +0200

    added api for the jokes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import discord
 from discord.ext import commands
 from apikeys import *
+from jokeapi import Jokes
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 client = commands.Bot(command_prefix='!', intents=intents) # initializing instance of a bot and letting it know that its
-                                            # commands start with "!"
+                                            # commands start with "!"                                       
 
 @client.event
 async def on_ready():   # when bot is ready to start receiving commands it executes this function
@@ -24,8 +25,16 @@ async def hello(ctx):
     await ctx.send("Hello, I am damrok's bot")
 
 @client.command()
-async def tell_joke(ctx):
-    await ctx.send("joke")
+async def joke(ctx):
+    jokeObj = await Jokes()
+    joke = await jokeObj.get_joke(category=["dark"])
+    if joke["type"] == "single": # Print the joke
+        await ctx.send(joke["joke"])
+    else:
+        await ctx.send(joke["setup"])
+        await ctx.send(joke["delivery"])
 
+    
+     
 client.run(BOTTOKEN)
 
